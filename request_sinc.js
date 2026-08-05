c38e7de8ef7adfb3e84eda8f0447329409292173
luchos-git <trevissan.luciano@gmail.com>
2026-08-05 20:31:36 -0300

    agregamos bugs

--- request_sinc.js
+++ request_sinc.js
@@ -6,10 +6,12 @@ fetch('https://6a73b2b015e0453fe1b424ed.mockapi.io/personas', {
       return res.json();
   }
   // handle error
-}).then(items => {
-  // Do something with the list of tasks
-  console.log(items);
-}).catch(error => {
+}).then(items => items.forEach(p => {
+
+//   console.log(item);
+  document.querySelector('.registros').innerHTML='<p>Nombre: ${p.nombre}</p><p>Direccion: ${p.direccion}</p><img src="${p.avatar}" alt="avatar" width="100"><hr></hr>'
+})
+).catch(error => {
   // handle error
   console.error('Error fetching personas:', error);
 })
