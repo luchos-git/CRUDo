9556540980ff41b5bb5dc2c9871f1fdc31420354
luchos-git <trevissan.luciano@gmail.com>
2026-08-10 19:16:39 -0300

    obteniendo un registro a base del id, genial que se hizo a la primera JAJAJ

--- request_sinc.js
+++ request_sinc.js
@@ -1,17 +1,35 @@
-fetch('https://6a73b2b015e0453fe1b424ed.mockapi.io/personas', {
+//1. 
+// fetch('https://6a73b2b015e0453fe1b424ed.mockapi.io/personas', {
+//   method: 'GET',
+//   headers: {'content-type':'application/json'},
+// }).then(Response => {
+//   if (Response.ok) {
+//       return Response.json();
+//   }
+// }).then(Items => Items.forEach(p => {
+
+//   console.log(p);
+//   document.querySelector('.registros').innerHTML+=`<p>Nombre: ${p.nombre}</p> <p>Direccion: ${p.direccion}</p> <img src="${p.imagen}" alt="imagen" width="100"><hr>`;
+// })
+// ).catch(error => {
+//   // handle error
+//   console.error('Error fetching personas:', error);
+// }
+// )
+
+//2. obtener un registro a base de un id
+fetch('https://6a73b2b015e0453fe1b424ed.mockapi.io/personas/2', { 
   method: 'GET',
   headers: {'content-type':'application/json'},
 }).then(Response => {
   if (Response.ok) {
       return Response.json();
   }
-}).then(Item => Item.forEach(p => {
-
-  console.log(p);
-  document.querySelector('.registros').innerHTML+=`<p>Nombre: ${p.nombre}</p> <p>Direccion: ${p.direccion}</p> <img src="${p.imagen}" alt="imagen" width="100"><hr>`;
-})
-).catch(error => {
+}).then(Item => {
+  console.log(Item);
+  document.querySelector('.registros').innerHTML+=`<p>Nombre: ${Item.nombre}</p> <p>Direccion: ${Item.direccion}</p> <img src="${Item.imagen}" alt="imagen" width="100"><hr>`;
+}).catch(error => {
   // handle error
-  console.error('Error fetching personas:', error);
+  console.error('Error fetching persona:', error);
 }
 )
